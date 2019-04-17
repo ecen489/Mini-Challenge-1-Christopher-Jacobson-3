02c9e31d0cb220810885ad131a4e49c2bfa04d55
Christopher Jacobson <christopherjacobson@tamu.edu>
2019-04-17 14:37:03 -0500

    MP9

--- app/src/main/java/com/example/minichallenge/MainActivity.java
+++ app/src/main/java/com/example/minichallenge/MainActivity.java
@@ -8,82 +8,179 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+//import com.google.firebase.ml.custom.FirebaseModelManager;
+import com.google.firebase.ml.custom.FirebaseModelOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
+//import com.google.firebase.ml.custom.model.FirebaseCloudModelSource;
+//import com.google.firebase.ml.custom.model.FirebaseLocalModelSource;
+//import com.google.firebase.ml.custom.model.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
+import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     private static final int REQ_CODE_TAKE_PICTURE = 0;
     ImageView img;
-    SQLiteDatabase db;
-    EditText idQ;
+    Bitmap mSelectedImage;
+    ListView listView;
+    ArrayList<String> labels;
+    ListAdapter labelsAdapter;
+
+
+    private static final int RESULTS_TO_SHOW = 3;
+
+    private final PriorityQueue<Map.Entry<String, Float>> sortedLabels =
+            new PriorityQueue<>(
+                    RESULTS_TO_SHOW,
+                    new Comparator<Map.Entry<String, Float>>() {
+                        @Override
+                        public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float>
+                                o2) {
+                            return (o1.getValue()).compareTo(o2.getValue());
+                        }
+                    });
+
+    private FirebaseModelInterpreter mInterpreter;
+    /**
+     * Data configuration of input & output data of model.
+     */
+    private FirebaseModelInputOutputOptions mDataOptions;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        idQ = (EditText) findViewById(R.id.etID);
-        Button btnCamera = (Button)findViewById(R.id.button);
-        Button btnPhoto = (Button)findViewById(R.id.btnDisplay);
+
+        labels = new ArrayList<String>();
+        labelsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, labels);
+        ListView listView = findViewById(R.id.lvLabels);
+        listView.setAdapter(labelsAdapter);
+        Button btnCamera = (Button) findViewById(R.id.button);
+        Button btnPhoto = (Button) findViewById(R.id.btnDisplay);
         img = (ImageView) findViewById(R.id.camera_image);
-        db = openOrCreateDatabase("Photos", MODE_PRIVATE, null);
-        String query = "CREATE TABLE IF NOT EXISTS Photos ( "
-                +  "  id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                +  "  photo BLOB NOT NULL "
-                +  ");";
-        db.execSQL(query);
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent  picIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                Intent picIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(picIntent, REQ_CODE_TAKE_PICTURE);
-                }
+            }
         });
 
 
         btnPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int currentID = Integer.parseInt(idQ.getText().toString());
-
-                Cursor cr;
-                byte[] getBMP = null;
-                Bitmap bitmap = null;
-                cr = db.rawQuery("SELECT photo FROM Photos WHERE id = " + currentID + ";", null);
-                if (cr.moveToFirst()) {
-                    getBMP = cr.getBlob(cr.getColumnIndex("photo"));
-                }
-                cr.close();
-                bitmap = BitmapFactory.decodeByteArray(getBMP, 0, getBMP.length);
-                img.setImageBitmap(bitmap);
-
+                runTextRecognition();
             }
         });
-
 
 
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (requestCode == REQ_CODE_TAKE_PICTURE && resultCode == RESULT_OK){
-            super.onActivityResult(requestCode,resultCode,data);
-            Bitmap bmp = (Bitmap) data.getExtras().get("data");
-            img.setImageBitmap(bmp);
-            byte[] bmpArray;
-            ByteArrayOutputStream bStream = new ByteArrayOutputStream();
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, bStream);
-            bmpArray = bStream.toByteArray();
-            ContentValues cv = new ContentValues();
-            cv.put("Photos", bmpArray);
-            db.insert("Photos", null, cv);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQ_CODE_TAKE_PICTURE && resultCode == RESULT_OK) {
+            super.onActivityResult(requestCode, resultCode, data);
+            mSelectedImage = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(mSelectedImage);
+            //byte[] bmpArray;
+            //ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+            //mSelectedImage.compress(Bitmap.CompressFormat.PNG, 100, bStream);
+            //bmpArray = bStream.toByteArray();
+
         }
+    }
+
+
+    private void runTextRecognition() {
+        //byte[] getBMP = null;
+        //Bitmap bitmap = null;
+        //bitmap = BitmapFactory.decodeByteArray(getBMP, 0, getBMP.length);
+        //img.setImageBitmap(bitmap);
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
+        FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        //mTextButton.setEnabled(false);
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                                //mTextButton.setEnabled(true);
+                                processTextRecognitionResult(texts);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                //mTextButton.setEnabled(true);
+                                e.printStackTrace();
+                            }
+                        });
+    }
+
+    private void processTextRecognitionResult(FirebaseVisionText texts) {
+        List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+        if (blocks.size() == 0) {
+            showToast("No text found");
+            return;
+        }
+        labels.clear();
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    labels.add(elements.get(k).getText());
+                }
+            }
+        }
+        labelsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, labels);
+        ListView listView = findViewById(R.id.lvLabels);
+        listView.setAdapter(labelsAdapter);
+    }
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
